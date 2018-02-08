b37a3f50092847be0b5727438a47904913e89822
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2018-02-08 10:28:14 -0500

    - [x] Update Machina core to 0.5.0 - [x] ICONS!! :) - [~] Make API names and descriptions consistent with other APIs - [~] Split nodes into classes

--- docs/MachinaDynamo_DEVLOG.md
+++ docs/MachinaDynamo_DEVLOG.md
@@ -27,6 +27,26 @@
 
 ---
 # REVISIONS
+## v.0.5.0
+- [x] Update Machina core to 0.5.0
+- [x] ICONS!! :)
+- [~] Split nodes into classes
+
+- [ ] Hide most pre-0.5.0 components
+- [ ] Remove all obsolete components, and create new ones with GUID to avoid overwrite
+- [ ] Make components have the option to choose between abs/rel
+- [ ] Rename `Motion` to `MotionMode`
+- [ ] Rename `FeedRate` to `ExtrusionRate`
+- [ ] Add GH_MutableComponent middleware class
+- [ ] Split component classes per file
+- [ ] Generate new components with new GuiD
+- [ ] Rename `CreateRobot` to `NewRobot` , same for `NewTool`
+- [ ] Mutable components now accept default values
+- [ ] Add namespaces to components
+- [ ] Add `Temperature`
+- [ ] Add `Extrude`
+- [ ] Add `ExtrusionRate`
+- [ ] Update sample files
 
 ## v.0.4.1
 - [x] Add 3D Printing sample
---
+++ src/MachinaDynamo/Robots.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Interfaces;
+using Autodesk.DesignScript.Geometry;
+
+using Machina;
+using MAction = Machina.Action;
+using MPoint = Machina.Point;
+using MVector = Machina.Vector;
+using MOrientation = Machina.Orientation;
+using MTool = Machina.Tool;
+using System.IO;
+
+namespace MachinaDynamo
+{
+    //  ██████╗  ██████╗ ██████╗  ██████╗ ████████╗███████╗
+    //  ██╔══██╗██╔═══██╗██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝
+    //  ██████╔╝██║   ██║██████╔╝██║   ██║   ██║   ███████╗
+    //  ██╔══██╗██║   ██║██╔══██╗██║   ██║   ██║   ╚════██║
+    //  ██║  ██║╚██████╔╝██████╔╝╚██████╔╝   ██║   ███████║
+    //  ╚═╝  ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝
+    //                                                     
+    /// <summary>
+    /// Robot creation and manipulation tools.
+    /// </summary>
+    public class Robots
+    {
+        /// <summary>
+        /// This API's version
+        /// </summary>
+        public static string MachinaDynamoAPIVersion() => "v.0.5.0";
+
+        /// <summary>
+        /// Make the constructor internal to hide it from the menu.
+        /// </summary>
+        internal Robots() { }
+
+        /// <summary>
+        /// Creates a new Robot instance.
+        /// </summary>
+        /// <param name="name">A name for this Robot</param>
+        /// <param name="brand">Input "ABB", "UR", "KUKA", "Zmoprh" or "HUMAN" (if you only need a human-readable representation of the actions of this Robot...)</param>
+        /// <returns name="Robot">Your brand new Machina Robot object</returns>
+        public static Robot New(string name = "MachinaRobot", string brand = "HUMAN") => new Robot(name, brand);
+
+        /// <summary>
+        /// Returns version and build numbers of the Machina Core library and Dynamo API.
+        /// </summary>
+        /// <returns></returns>
+        [MultiReturn(new[] { "Core", "Dynamo API" })]
+        public static Dictionary<string, string> Version() => new Dictionary<string, string> {
+            { "Core", Robot.Version },
+            { "Dynamo API", Robots.MachinaDynamoAPIVersion()}
+        };
+
+        /// <summary>
+        /// Change the name of a Robot's IO pin.
+        /// </summary>
+        /// <param name="bot">Robot to change the io name to</param>
+        /// <param name="name">New IO name</param>
+        /// <param name="pin">Pin number</param>
+        /// <param name="digital">Is this a digital pin?</param>
+        /// <returns name="Robot">Robot with named IO</returns>
+        public static Robot SetIOName(Robot bot, string name = "Digital_IO_1", int pin = 1, bool digital = true)
+        {
+            bot.SetIOName(name, pin, digital);
+            return bot;
+        }
+
+    }
+}
